e87df262cc4effa3b0ba03c2532596c8332b231a
mailtomainak <mailtomainak@gmail.com>
2019-01-07 20:04:01 +0530

    solve targetSumExistsInTwoNumbers

--- target-sum-exists/target-sum-exists.js
+++ target-sum-exists/target-sum-exists.js
@@ -1,5 +1,19 @@
 const targetSumExistsInTwoNumbers = (list, target) => {
-  return false
+  
+  const inputSpace = [...list,...list.map(number=>number*2)];
+  const inputSpaceLength = inputSpace.length;
+  let possibleCombinations=[];
+
+  //loop inside a loop thankfully its a smaller array
+  for(var i=0;i<inputSpaceLength;i++){
+    for(var j=0;j<inputSpaceLength;j++){
+      if(j!==i){
+      possibleCombinations.push(inputSpace[i]+inputSpace[j]);
+      }
+    }
+  }
+  
+  return possibleCombinations.filter(num=>num===target).length!==0?true:false
 }
 
 const targetSumExistsInThreeNumbers = (list, target) => {
